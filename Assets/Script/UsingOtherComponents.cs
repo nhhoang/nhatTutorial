2f3bb99befbcbe757f2e41f5c124807b2318a5bb
nhatnd <nhatnd@Nguyens-MacBook-Pro.local>
2014-11-26 00:18:01 +0700

    add scene 20

---
+++ Assets/Script/UsingOtherComponents.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UsingOtherComponents : MonoBehaviour {
+
+	public GameObject otherGameObject;
+
+	public ScopeAndAccessModifiers anotherScript;
+
+	private ScopeAndAccessModifiers anotherScript1;
+
+	void Start() {
+		Debug.Log(anotherScript.alpha);
+
+		Debug.Log(otherGameObject.GetComponent<ScopeAndAccessModifiers>().alpha);
+
+		anotherScript1 = GetComponent<ScopeAndAccessModifiers>();
+		Debug.Log(anotherScript1.alpha);	
+
+	}
+	
+	void Update() {
+	
+	}
+}
